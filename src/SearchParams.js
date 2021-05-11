93def35330595477b03cfdb94b10a77241ac5369
Eric Allen Youngson <77707579+ericyoungson@users.noreply.github.com>
2021-05-11 07:17:18 -0700

    convert to JSX, add linting

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
 
 const SearchParams = () => {
-  const location = "Seattle, WA";
+  const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("dog");
 
-  return(
+  return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
           Location
-          <input id="location" value={location} placeholder="Location" />
+          <input id="location" value={location}
+          placeholder="Location" onChange={event => setLocation(event.target.value)} />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={event => setAnimal(event.target.value)}
+            onBlur={event => setAnimal(event.target.value)}>
+            <option>All</option>
+            {ANIMALS.map(animal => (
+              <option key={animal} value={animal}>{animal}</option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
