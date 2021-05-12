9739958a99b8f32499dd21f139b73feb673ad716
Eric Allen Youngson <77707579+ericyoungson@users.noreply.github.com>
2021-05-11 18:46:08 -0700

    Added dropdown for animal type

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -10,26 +10,33 @@ const SearchParams = () => {
       <form>
         <label htmlFor="location">
           Location
-          <input id="location" value={location}
-          placeholder="Location" onChange={event => setLocation(event.target.value)} />
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(event) => setLocation(event.target.value)}
+          />
         </label>
         <label htmlFor="animal">
           Animal
           <select
             id="animal"
             value={animal}
-            onChange={event => setAnimal(event.target.value)}
-            onBlur={event => setAnimal(event.target.value)}>
+            onChange={(event) => setAnimal(event.target.value)}
+            onBlur={(event) => setAnimal(event.target.value)}
+          >
             <option>All</option>
-            {ANIMALS.map(animal => (
-              <option key={animal} value={animal}>{animal}</option>
+            {ANIMALS.map((animal) => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
             ))}
           </select>
         </label>
         <button>Submit</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
 export default SearchParams;
