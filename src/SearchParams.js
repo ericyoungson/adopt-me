ab4a83a0677eb844f0c4b22cf189f399819b998c
Eric Allen Youngson <77707579+ericyoungson@users.noreply.github.com>
2021-05-12 14:48:44 -0700

    Moved dropdown selections to reusable component

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
 import { ANIMALS } from "@frontendmasters/pet";
+import DropdownComponenet from "./DropdownComponent";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
-  const [animal, setAnimal] = useState("dog");
+  const [breeds, setBreeds] = useState([])
+  const [animal, AnimalDropdown] = DropdownComponenet("Animal", "Dog", ANIMALS);
+  const [breed, BreedDropdown] = DropdownComponenet("Breed", "", breeds)
 
   return (
     <div className="search-params">
@@ -17,22 +20,8 @@ const SearchParams = () => {
             onChange={(event) => setLocation(event.target.value)}
           />
         </label>
-        <label htmlFor="animal">
-          Animal
-          <select
-            id="animal"
-            value={animal}
-            onChange={(event) => setAnimal(event.target.value)}
-            onBlur={(event) => setAnimal(event.target.value)}
-          >
-            <option>All</option>
-            {ANIMALS.map((animal) => (
-              <option key={animal} value={animal}>
-                {animal}
-              </option>
-            ))}
-          </select>
-        </label>
+        <AnimalDropdown />
+        <BreedDropdown />
         <button>Submit</button>
       </form>
     </div>
